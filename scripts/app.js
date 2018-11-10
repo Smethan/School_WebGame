9f4d0b8bdeeb22d2da2cf1d1b54c396ee127758e
smethan <ethanhargus@gmail.com>
2018-11-10 14:35:23 -0600

    switched to phaser.js because I'm an idiot. Time to actually begin development

---
+++ scripts/app.js
@@ -0,0 +1,82 @@
+
+// import * as PIXI from 'pixi.min.js';
+// const PIXI = require(['pixi.js'], function(pixi){});
+let type = "WebGL";
+if(!PIXI.utils.isWebGLSupported()){
+    type = "canvas"
+}
+PIXI.utils.sayHello(type);
+let app = new PIXI.Application({width: 256, height: 256, antialias: true, transparent: false, resolution: 1});
+app.stage.interactive = true;
+app.renderer.backgroundColor = 0x061639;
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
+app.renderer.autoResize = true;
+app.renderer.resize(window.innerWidth, window.innerHeight);
+document.body.appendChild(app.view);
+PIXI.loader.add("images/Cat_test.png").load(setup);
+var sprite;
+function setup(){
+    var rect =  new PIXI.Rectangle(0,0,50,50);
+    var texture = PIXI.loader.resources["images/Cat_test.png"].texture;
+    texture.frame = rect;
+    sprite = new PIXI.Sprite(texture);
+
+    var idle = setInterval(function(){
+        if (rect.x >= 64 * 4) rect.x = 0;
+        sprite.texture.frame = rect;
+        rect.x += 64;
+    }, 500);
+
+    sprite.vx = 10;
+    sprite.vy = 10;
+    // sprite.scale.set(2,2);
+    app.stage.addChild(sprite);
+    window.addEventListener("keydown", function(event){
+        event.preventDefault();
+        if (event.keyCode == 37) {sprite.x -= sprite.vx}
+        if (event.keyCode == 38) {sprite.y -= sprite.vy}
+        if (event.keyCode == 39) {sprite.x += sprite.vx}
+        if (event.keyCode == 40) {sprite.y += sprite.vy}
+
+    })
+
+
+
+}
+
+
+// let bigRect = new PIXI.Graphics();
+//
+// bigRect.beginFill(0x00ff00);
+//
+// bigRect.drawRect(0,0,50,50);
+// bigRect.interactive = true;
+// bigRect.buttonMode = true;
+// bigRect
+//     .on('pointerdown', pdown)
+//     .on('pointerup', pup)
+//     .on('pointerupoutside', pup)
+//     .on('pointermove', pdrag);
+// function pdown(event){
+//     this.data = event.data;
+//     this.alpha = 0.5;
+//     this.dragging = true;
+// }
+//
+// function pup(event){
+//     this.alpha = 1;
+//     this.dragging = false;
+//     this.data = null;
+// }
+//
+// function pdrag(event){
+//     if (this.dragging){
+//         var newposition = this.data.getLocalPosition(this.parent);
+//         this.x = newposition.x - 25;
+//         this.y = newposition.y - 25;
+//     }
+// }
+// bigRect.hitArea = bigRect.getBounds();
+// app.stage.addChild(bigRect);
+//
